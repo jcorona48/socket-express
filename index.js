28245a22971ed7561adbf6c86c2e0dfe5fb0b469
Joan Corona <joaneliasc86@gmail.com>
2023-12-20 01:12:47 -0400

    add group users

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const svr = createServer(app);
 const io = new Server(svr);
 
+let users = [];
+
 const port = 3000;
 
 app.get("/", (req, res) => {
@@ -13,15 +15,28 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
     console.log("a user connected");
+
+    socket.on("join", ({ name }) => {
+        users.push({ id: socket.id, name });
+        console.log(users);
+        io.emit("join", users);
+    });
+
     socket.on("message", (msg) => {
         console.log(msg);
         const { message, name } = msg;
         console.log(socket.id);
         io.emit("message", { msg: message, id: socket.id, name });
     });
+
+    socket.on("disconnect", () => {
+        users = users.filter((user) => user.id !== socket.id);
+        console.log("user disconnected");
+    });
 });
 
-io.on("disconnect", () => {
+io.on("disconnect", (socket) => {
+    users = users.filter((user) => user.id !== socket.id);
     console.log("user disconnected");
 });
 
